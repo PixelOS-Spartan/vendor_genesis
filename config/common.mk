1423ac1b2fb9c38111c34d4acca22442a7b048e6
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    common: Switch to AOSPA's face unlock implementation

--- config/common.mk
+++ config/common.mk
@@ -94,6 +94,18 @@ PRODUCT_PACKAGES += \
     product_charger_res_images \
     product_charger_res_images_vendor
 
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+# FaceUnlock
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 ifeq ($(GENESIS_OFFICIAL), true)
 # Genesis packages
 PRODUCT_PACKAGES += \
